1b0f58e04eb9e3c5d90aeebd07a89b5e39c8138e
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-07-09 15:26:47 +0200

    Latest

---
+++ testing.js
@@ -0,0 +1,22 @@
+function greetNormal(str) {
+    console.log(str);
+}
+
+greetNormal("normal");
+
+
+let greetAnonymous = function(str) {
+    console.log(str);
+}
+
+greetAnonymous("anom");
+
+
+let arrowFunction = (arrowFunctionInput) {
+    console.log(arrowFunctionInput);
+}
+
+
+arrowFunction("THis is an arrow function");
+//setTimeout(greetAnonymous("setTimeoutFunction"), 4000);
+
